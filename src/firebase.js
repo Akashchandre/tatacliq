d1d389fc7f376e57c135de9b4288c6034410a5e3
Akashchandre <chandreakash@gmail.com>
2025-01-29 16:38:27 +0530

    env

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 const firebaseConfig = {
-    apiKey: "AIzaSyBwsxWjZROtw0LgHx2zZzk2dUVupsFHD0c",
-    authDomain: "tatacliq-8be1b.firebaseapp.com",
-    projectId: "tatacliq-8be1b",
-    storageBucket: "tatacliq-8be1b.firebasestorage.app",
-    messagingSenderId: "178116225292",
-    appId: "1:178116225292:web:bacff33b4f09544671c129",
-    measurementId: "G-DQVEZHTS51"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
